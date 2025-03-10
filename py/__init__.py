f578f4b3401332b76e86b1789e322243a43b4b16
jupo-ai <jupojupo.ai@gmail.com>
2025-03-10 11:33:44 +0900

    add hyper multiplier

--- py/__init__.py
+++ py/__init__.py
@@ -1,8 +1,10 @@
 from . import block_sliders
-from . import exp_preset_slider
+from . import blocks_preset_sliders
 from . import blocks_updater
+from . import hyper_multiplier
 
 NODE_CLASS_MAPPINGS = {}
 NODE_CLASS_MAPPINGS.update(block_sliders.NODE_CLASS_MAPPINGS)
-NODE_CLASS_MAPPINGS.update(exp_preset_slider.NODE_CLASS_MAPPINGS)
+NODE_CLASS_MAPPINGS.update(blocks_preset_sliders.NODE_CLASS_MAPPINGS)
 NODE_CLASS_MAPPINGS.update(blocks_updater.NODE_CLASS_MAPPINGS)
+NODE_CLASS_MAPPINGS.update(hyper_multiplier.NODE_CLASS_MAPPINGS)
